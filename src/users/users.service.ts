3b35936c4cfa62ffada8754db118e975c12f219b
alex <alex.karamushko@nordclan.com>
2021-05-25 21:15:58 +0300

    upd(roles): send user with role

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { hash } from 'bcrypt';
+import { Role } from 'common/types/role';
 import { Repository } from 'typeorm';
 import { CreateUserRequestDTO } from './dto';
 import { User } from './user.entity';
@@ -13,20 +14,20 @@ export class UsersService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
-  async findOneByName(username: string): Promise<User> {
+  async findOneByName(username: string): Promise<User | undefined> {
     return await this.userRepository.findOne({
       where: { username }
     });
   }
 
-  async create({ username, password, role }: CreateUserRequestDTO): Promise<User> {
+  async create({ username, password }: CreateUserRequestDTO): Promise<User | undefined> {
     const newUser = new User();
 
     const hashPassword = await hash(password, 10);
 
     newUser.username = username;
     newUser.password = hashPassword;
-    newUser.role = role;
+    newUser.role = Role.USER;
 
     const createdUser = await this.userRepository.save(newUser);
 
